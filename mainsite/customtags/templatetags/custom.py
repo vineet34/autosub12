ec752cfe301dcb7bd1d65df7643a5b401e49237b
Vineet <vineet.1991.483@gmail.com>
2012-05-29 16:10:31 +0530

    view user using dajax

--- mainsite/customtags/templatetags/custom.py
+++ mainsite/customtags/templatetags/custom.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.auth.models import User
 
 register = template.Library()
 @register.simple_tag
@@ -8,3 +9,10 @@ def getFileURL(dict, key, qid):
 @register.simple_tag
 def getFileName(dict, key, qid):
 	return dict.get(str(qid)+'_'+str(key)+'_name', '')
+	
+@register.simple_tag
+def getProfileStatus(username):
+	if User.objects.filter(username=username)[0].is_active:
+		return 'Y'
+	else:
+		return 'N'
